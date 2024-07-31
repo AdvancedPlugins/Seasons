45e0eccbb38427806c266c0c766dbf9f54d60993
Ynverxe <nahubar65@gmail.com>
2024-07-31 08:41:24 -0300

    part of the commit 5785e48c9a4cfbd9ec96dd37a21dfd2d9428e59d

---
+++ src/main/java/net/advancedplugins/seasons/api/event/SeasonUpdateCause.java
@@ -0,0 +1,7 @@
+package net.advancedplugins.seasons.api.event;
+
+public enum SeasonUpdateCause {
+  NATURAL,
+  PLUGIN,
+  COMMAND;
+}
---
+++ src/main/java/net/advancedplugins/seasons/api/event/SeasonUpdateEvent.java
@@ -0,0 +1,46 @@
+package net.advancedplugins.seasons.api.event;
+
+import net.advancedplugins.seasons.api.calendar.WorldCalendar;
+import net.advancedplugins.seasons.api.season.SeasonSnapshot;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+import static java.util.Objects.requireNonNull;
+
+public class SeasonUpdateEvent extends AbstractCalendarEvent {
+
+  private static final HandlerList HANDLER_LIST = new HandlerList();
+
+  private final @NotNull SeasonUpdateCause cause;
+  private final @NotNull SeasonSnapshot oldState;
+  private final @NotNull SeasonSnapshot newState;
+
+  public SeasonUpdateEvent(@NotNull WorldCalendar calendar, @NotNull SeasonUpdateCause cause, @NotNull SeasonSnapshot oldState, @NotNull SeasonSnapshot newState) {
+    super(calendar);
+    this.oldState = requireNonNull(oldState, "oldState");
+    this.newState = requireNonNull(newState, "newState");
+    this.cause = requireNonNull(cause, "cause");
+  }
+
+  public @NotNull SeasonSnapshot oldState() {
+    return oldState;
+  }
+
+  public @NotNull SeasonSnapshot newState() {
+    return newState;
+  }
+
+  public @NotNull SeasonUpdateCause cause() {
+    return cause;
+  }
+
+  @NotNull
+  @Override
+  public HandlerList getHandlers() {
+    return HANDLER_LIST;
+  }
+
+  public static HandlerList getHandlerList() {
+    return HANDLER_LIST;
+  }
+}
