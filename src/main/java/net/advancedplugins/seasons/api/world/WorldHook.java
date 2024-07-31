6b7c62386d8eb9310d5ecb04d9e476792153d812
Ynverxe <nahubar65@gmail.com>
2024-07-31 10:44:23 -0300

    fix: add NotCache annotation

--- src/main/java/net/advancedplugins/seasons/api/world/WorldHook.java
+++ src/main/java/net/advancedplugins/seasons/api/world/WorldHook.java
@@ -1,10 +1,12 @@
 package net.advancedplugins.seasons.api.world;
 
+import net.advancedplugins.seasons.api.annotation.NotCache;
 import net.advancedplugins.seasons.api.calendar.WorldCalendar;
 import net.advancedplugins.seasons.api.calendar.configuration.CalendarConfiguration;
 import org.bukkit.World;
 import org.jetbrains.annotations.NotNull;
 
+@NotCache
 public interface WorldHook {
 
   @NotNull World world();
