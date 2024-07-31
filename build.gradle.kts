9dbaa9e4f10f0cc86394acef8a9998a00d7b8138
Ynverxe <nahubar65@gmail.com>
2024-07-31 08:46:52 -0300

    ci: setup junit

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,20 @@ repositories {
 dependencies {
     compileOnly("org.jetbrains:annotations:24.0.0")
     compileOnly("org.spigotmc:spigot-api:1.20.1-R0.1-SNAPSHOT")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+
+    maxHeapSize = "1G"
+
+    testLogging {
+        events("passed")
+    }
 }
 
 publishing {
