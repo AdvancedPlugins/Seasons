d9f012d518dc59ef53e82bdaafb3f0d6639bb85c
Ynverxe <nahubar65@gmail.com>
2024-07-31 08:46:03 -0300

    feat: added methods to set year and year day, to skip calendar ticks, to skip season phases and to get the WorldCalendar of a World

--- src/main/java/net/advancedplugins/seasons/api/calendar/WorldCalendar.java
+++ src/main/java/net/advancedplugins/seasons/api/calendar/WorldCalendar.java
@@ -1,11 +1,20 @@
 package net.advancedplugins.seasons.api.calendar;
 
 import net.advancedplugins.seasons.api.annotation.NotCache;
+import net.advancedplugins.seasons.api.calendar.configuration.CalendarConfiguration;
 import net.advancedplugins.seasons.api.season.SeasonType;
+import net.advancedplugins.seasons.api.world.WorldHandler;
+import net.advancedplugins.seasons.api.world.WorldHook;
+import org.bukkit.World;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
 
 @NotCache
 public interface WorldCalendar {
+
+  @NotNull
+  World world();
 
   @NotNull
   CalendarConfiguration configuration();
@@ -13,6 +22,29 @@ public interface WorldCalendar {
   @NotNull
   CalendarSnapshot calendarSnapshot();
 
-  void setSeason(@NotNull SeasonType season);
+  boolean setSeason(@NotNull SeasonType season, int transition);
 
+  void advance();
+
+  void setYear(int year);
+
+  default void setYearDay(int yearDay) {
+    setYearDayIndex(yearDay - 1);
+  }
+
+  void setYearDayIndex(int yearDayIndex) throws IllegalArgumentException;
+
+  /**
+   * This method skips calendar loops until
+   * reach (or exceed) the world time goal.
+   *
+   * @param goal The goal to reach.
+   * @throws IllegalArgumentException If goal is less than 1L or major than 24000L
+   */
+  void tryReachWorldTime(long goal) throws IllegalArgumentException;
+
+  static @NotNull Optional<WorldCalendar> of(@NotNull World world) {
+    return WorldHandler.instance().findCached(world)
+        .map(WorldHook::calendar);
+  }
 }
