f4fc4ce260f0338284a121a11f9eb1c658f6ca72
Ynverxe <nahubar65@gmail.com>
2024-07-31 08:30:15 -0300

    feat: create an AdvancedSeasonsAssembly as an API core to expose primary handlers

---
+++ src/main/java/net/advancedplugins/seasons/api/AdvancedSeasonsAssembly.java
@@ -0,0 +1,28 @@
+package net.advancedplugins.seasons.api;
+
+import net.advancedplugins.seasons.api.biome.BiomeHandler;
+import net.advancedplugins.seasons.api.calendar.CalendarConfigurationHandler;
+import net.advancedplugins.seasons.api.temperature.TemperatureHandler;
+import net.advancedplugins.seasons.api.world.WorldHandler;
+import org.jetbrains.annotations.NotNull;
+
+public final class AdvancedSeasonsAssembly {
+
+  private AdvancedSeasonsAssembly() {}
+
+  public static @NotNull BiomeHandler biomeHandler() {
+    throw new IllegalArgumentException("Unimplemented");
+  }
+
+  public static @NotNull CalendarConfigurationHandler calendarConfigurationHandler() {
+    throw new IllegalArgumentException("Unimplemented");
+  }
+
+  public static @NotNull TemperatureHandler temperatureHandler() {
+    throw new IllegalArgumentException("Unimplemented");
+  }
+
+  public static @NotNull WorldHandler worldHandler() {
+    throw new IllegalArgumentException("Unimplemented");
+  }
+}
